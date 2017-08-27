27c9362ea4799fd9c460181b25853bf0db6f11be
Nathan Berry <nathanmberry@gmail.com>
2017-08-27 12:42:57 -0600

    Implement frontend test with mock MovieService

--- src/main/client/src/app/app.component.spec.ts
+++ src/main/client/src/app/app.component.spec.ts
@@ -1,32 +1,85 @@
-import { TestBed, async } from '@angular/core/testing';
+import {async, ComponentFixture, ComponentFixtureAutoDetect, TestBed} from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DebugElement} from "@angular/core";
+import {MovieService} from "./movies.service";
+import {By} from "@angular/platform-browser";
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/of";
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+    let movieService: MovieService;
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    beforeEach(async(() => {
+        let movieServiceStub = {
+            searchMovies: (search) => {
+                console.log("MovieService returning mock data for " + search);
+                return Observable.of({
+                    "Search": [
+                        {"imdbID": "1", "Title": "Newton 1", "Year": "2015", "Poster": "NewtonPoster1"},
+                        {"imdbID": "2", "Title": "Newton 2", "Year": "2016", "Poster": "NewtonPoster2"},
+                        {"imdbID": "3", "Title": "Newton 3", "Year": "2017", "Poster": "N/A"}
+                    ],
+                    "totalResults": "3"
+                });
+            }
+        };
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            providers: [
+                {provide: ComponentFixtureAutoDetect, useValue: true},
+                {provide: MovieService, useValue: movieServiceStub}
+            ]
+        }).compileComponents();
+    }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        movieService = TestBed.get(MovieService);
+    });
+
+    it('should create the app', async(() => {
+        expect(comp).toBeTruthy();
+        expect(comp.searchResults).toBeTruthy();
+
+        // query for the title <h1> by CSS element selector
+        de = fixture.debugElement.query(By.css('table'));
+        el = de.nativeElement;
+        let tBody: HTMLElement;
+        let rows: NodeListOf<HTMLElement>;
+        tBody = el.getElementsByTagName("tbody")[0];
+        rows = tBody.getElementsByTagName("tr");
+        expect(rows.length).toEqual(3);
+
+        assertMovie(rows, 0, "Newton 1", "2015", "NewtonPoster1");
+        assertMovie(rows, 1, "Newton 2", "2016", "NewtonPoster2");
+        assertMovie(rows, 2, "Newton 3", "2017", null);
+    }));
+
+    let assertMovie = function (rows: NodeListOf<HTMLElement>, index: number, expectedTitle: string,
+                                expectedYear: string, expectedPoster: string) {
+        let row: HTMLElement;
+        let cells: NodeListOf<HTMLElement>;
+        row = rows[index];
+        cells = row.getElementsByTagName("td");
+        expect(cells.length).toEqual(3);
+
+        if (expectedPoster != null) {
+            expect(cells[0].children.length).toEqual(1);
+            expect(cells[0].children[0].getAttribute("src")).toEqual(expectedPoster);
+        } else {
+            expect(cells[0].innerText).toContain("");
+        }
+
+        expect(cells[1].innerText).toContain(expectedTitle);
+        expect(cells[2].innerText).toContain(expectedYear);
+    };
 });
